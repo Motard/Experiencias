f83643e9d72e9ef1ed1fd56819fa3c0bac059a7c
paulo martins <pmotard@sapo.pt>
2014-02-25 15:15:38 +0000

    Calculadora Feita

--- Calculadora/src/com/example/calculadora/CalculadoraActivity.java
+++ Calculadora/src/com/example/calculadora/CalculadoraActivity.java
@@ -1,15 +1,25 @@
 package com.example.calculadora;
 
+import java.util.ArrayList;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CalculadoraActivity extends Activity {
+	
+	private TextView display;
+	private boolean novoInsert = true;
+	private ArrayList<Integer> numeros = new ArrayList<Integer>(); 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
+        
+        display = (TextView) findViewById(R.id.display);
     }
 
 
@@ -18,6 +28,50 @@ public class CalculadoraActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.calculadora, menu);
         return true;
+    }
+    
+    public void onNumberPressed(View view)
+    {
+    	// Cast a view do tipo View para o tipo Button para poder efectuar o metodo getText()
+    	String texto = ((Button) view).getText().toString();
+    	
+    	if(novoInsert)
+    	{
+    		display.setText(texto);
+    		novoInsert = false;
+    	}
+    	else
+    		display.setText(display.getText() + texto);
+    }
+    
+    public void onEnterPressed(View view)
+    {
+    	
+    	int numero1 = Integer.parseInt(display.getText().toString());
+    	numeros.add(numero1);
+    	
+    	novoInsert = true;
+    	display.setText("0");
+    
+    }
+    
+    public void onOperationPressed(View view)
+    {
+    	String operacao = ((Button) view).getText().toString();
+    	int posicoes = numeros.size();
+    	int resultado=0;
+    	
+    	if (operacao.equals("+"))
+    	{
+    		for (int i = 0; i<posicoes; i++)
+    		{
+    			resultado += numeros.get(i);
+    		}
+    	}
+    	
+    	display.setText(String.valueOf(resultado));
+    	
+    	numeros.clear();
     }
     
 }
